a4dc69fc2b6c7b8154edd23ddc42191eb42d39dd
akki1071993 <akashmahant143@gmail.com>
2022-05-18 17:47:56 +0530

    from the gitHub

--- src/myFristJava/demo.java
+++ src/myFristJava/demo.java
@@ -5,6 +5,7 @@ public class demo {
 	public static void main(String[] args) {
 		System.out.println("hello World !");	
 		System.out.println("I'm Akash Mahant!")
+		System.out.println("I'm Java Devloper")
 	}
 
 }
